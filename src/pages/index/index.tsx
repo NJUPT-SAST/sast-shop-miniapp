19d3565784cf865f280496463ccf2a648f40474d
Jiang-Ci123 <2937405728@qq.com>
2023-05-15 01:48:22 +0800

    0515 index传参

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,46 +1,74 @@
-import { View, Text, Input, Image } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import input_icon from '../../assets/images/index/find.png'
+import { View, Text, Image } from '@tarojs/components'
+import Taro, { useLoad, useRouter } from '@tarojs/taro'
+import { useState, useEffect } from 'react'
 import item_image from '../../assets/images/index/frame.png'
 import './index.scss'
 
 export default function Index() {
+  const [title, changeTitle] = useState('南京邮电大学校科协 SAST 2023 纪念定制短袖')
+  const [price, changePrice] = useState('99')
+  const [id, changeId] = useState('0')
+  const [image, changeImage] = useState(item_image)
+  const [indexObject, changeindexObject] = useState([{
+    title: '南京邮电大学校科协 SAST 2023 纪念定制短袖',
+    price: 99,
+    image: item_image,
+    id: 0
+  }])
+
+  const gotoDetail = () => {
+    Taro.navigateTo({
+      url: `/pages/product-detail/index?id=${id}`
+    })
+  }
+
+
+  useEffect(() => {
+    Taro.request({
+      url: 'http://127.0.0.1:4523/m1/2655521-0-default/user/product',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        // console.log(res.data.image)
+        changeTitle(res.data.title)
+        changePrice(res.data.price)
+        changeId(res.data.id)
+        changeImage(res.data.image)
+        changeindexObject([{
+          title: res.data.title,
+          price: res.data.price,
+          image: res.data.image,
+          id: res.data.id
+        }])
+      }
+    })
+  }, [])
 
   useLoad(() => {
-    console.log('Page loaded.')
+    // console.log('Page loaded.')
   })
 
   return (
     <View className='index'>
-      <View className='index-title'>
-        <Text>商品列表</Text>
-      </View>
 
-      <View className='index-input'>
-        <View className='input-image'>
-          <Image mode='aspectFit' style='width: 25px;height: 25px' className='input-icon' src={input_icon}></Image>
-        </View>
-        <View className='input-text'>
-          <Input type='text' placeholder='搜索想要的商品' placeholderStyle='color: #8a8a8a;' className='input' />
-        </View>
-      </View>
-
-      <View className='index-body'>
-        <View className='body-item'>
-          <View className='item-image'>
-            <Image mode='aspectFit' className='picture' style='height: 100px' src={item_image}></Image>
-          </View>
-          <View className='item-text'>
-            <View className='text-title'>
-              <Text>南京邮电大学校科协 SAST 2023 纪念定制短袖</Text>
+      {
+        indexObject.map(({ title, price, id, image }) => {
+          return <View className='body-item' key={id} onClick={gotoDetail}>
+            <View className='item-image'>
+              <Image mode='aspectFit' className='picture' style='height: 100px' src={image}></Image>
             </View>
-            <View className='text-money'>
-              ￥99.00
+            <View className='item-text'>
+              <View className='text-title'>
+                <Text>{title}</Text>
+              </View>
+              <View className='text-money'>
+                ￥{price}
+              </View>
             </View>
           </View>
-        </View>
-      </View>
-
+        })
+      }
 
     </View>
   )
--- src/pages/product-detail/index.tsx
+++ src/pages/product-detail/index.tsx
@@ -1,5 +1,5 @@
-import { View, Swiper, SwiperItem, Image, Button, ScrollView,Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Swiper, SwiperItem, Image, Button, ScrollView, Text } from '@tarojs/components'
+import { useLoad, getCurrentInstance } from '@tarojs/taro'
 import img1 from '../../assets/images/img1.jpg'
 import img2 from '../../assets/images/img2.jpg'
 import img3 from '../../assets/images/img3.jpg'
@@ -10,12 +10,15 @@ import './index.scss'
 export default function ProductDetail() {
 
   useLoad(() => {
-    console.log('Page loaded.')
+    // console.log('Page loaded.')
   })
+
+  const id = getCurrentInstance().router.params.id
+  console.log(id)
 
   return (
     <View
-    style='overflow:hidden'
+      style='overflow:hidden'
     >
       <Swiper
         className='swiper'
@@ -24,35 +27,35 @@ export default function ProductDetail() {
         circular
         indicatorDots
         display-multiple-items
-        >
+      >
         <SwiperItem className='swiper-item'><Image mode='aspectFit' style='width:100%;height:100% ' className='img-item' src={img1}></Image></SwiperItem>
         <SwiperItem className='swiper-item'><Image mode='aspectFit' style='width:100%;height:100% ' className='img-item' src={img2}></Image></SwiperItem>
         <SwiperItem className='swiper-item'><Image mode='aspectFit' style='width:100%;height:100% ' className='img-item' src={img3}></Image></SwiperItem>
         <SwiperItem className='swiper-item'><Image mode='aspectFit' style='width:100%;height:100% ' className='img-item' src={img4}></Image></SwiperItem>
       </Swiper>
-        <View className="good-introduce">南京邮电大学校科协 SAST 2023 纪念定制短袖</View>
-        <View className="good-price">￥99.00</View>
-        <Button className='good-button'>立即购买</Button>
+      <View className="good-introduce">南京邮电大学校科协 SAST 2023 纪念定制短袖</View>
+      <View className="good-price">￥99.00</View>
+      <Button className='good-button'>立即购买</Button>
 
-        <View className='type'>
-          <View className='type-view'>颜色<Text className='type-item'>卡其色 深蓝色</Text></View>
-          <View className='type-view'>尺码<Text className='type-item'>S M L XL 2XL 3XL 4XL</Text></View>
+      <View className='type'>
+        <View className='type-view'>颜色<Text className='type-item'>卡其色 深蓝色</Text></View>
+        <View className='type-view'>尺码<Text className='type-item'>S M L XL 2XL 3XL 4XL</Text></View>
+      </View>
+
+      <View className='datail'>图片详情</View>
+
+      <ScrollView
+        className='scrollview'
+        scrollY
+        lowerThreshold={50}
+        style='height:1200px width:100%'
+        showScrollbar={false}
+        enhanced={true}
+      >
+        <View className='long-picture'>
+          <Image className='detail-picture' src={detail} mode='widthFix'></Image>
         </View>
-
-        <View className='datail'>图片详情</View>
-
-        <ScrollView
-          className='scrollview'
-          scrollY
-          lowerThreshold={50}
-          style='height:1200px width:100%'
-          showScrollbar={false}
-          enhanced={true}
-        >
-          <View className='long-picture'>
-            <Image className='detail-picture' src={detail} mode='widthFix'></Image>
-          </View>
-        </ScrollView>
+      </ScrollView>
     </View>
   )
 }
